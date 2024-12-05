b1620e52ca3e0098f24900cf073a0da579fc8b06
Syaifudinnn <mukhammad_adn_30rpl@student.smktelkom-mlg.sch.id>
2024-12-05 14:19:02 +0700

    add tambahDataKRS()

--- studycase_16_23.java
+++ studycase_16_23.java
@@ -16,8 +16,9 @@ public class studycase_16_23 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean isRun = true;
 
-        while (true) {
+        while (isRun) {
             header();
             int pilihMenu = input.nextInt();
 
@@ -34,15 +35,17 @@ public class studycase_16_23 {
                 case 4:
                     System.out.println("Program Selesai");
                     System.out.println("Terima Kasih!");
+                    isRun = false;
                     break;
                 default:
-                    break;
+                    System.out.println("Pilihan tidak valid!");
             }
+
         }
     }
 
     public static void header() {
-        System.out.println("=== PENCATATAN PRESTASI MAHASISWA ===");
+        System.out.println("\n=== PENCATATAN PRESTASI MAHASISWA ===");
         System.out.println("1. Tambah Data KRS");
         System.out.println("2. Tampilkan Daftar KRS Mahasiswa");
         System.out.println("3. Analisis Data KRS ");
@@ -51,7 +54,74 @@ public class studycase_16_23 {
     }
 
     public static void tambahDataKRS() {
+        Scanner input = new Scanner(System.in);
 
+        //cek apakah data mahasiswa mencapai limit
+        if (jumlahMhs >= max_mhs) {
+            System.out.println("Kapasistas mahasiswa penuh");
+            return;
+        }
+
+        System.out.println("\n--- Tambah Data KRS ---");
+
+        System.out.print("Nama Mahasiswa : ");
+        String nama = input.nextLine();
+        System.out.print("NIM : ");
+        String nim = input.nextLine();
+
+        namaMhs[jumlahMhs] = nama; //simpan nama yang diinput ke array namaMHS
+        nimMhs[jumlahMhs] = nim; //simpan nim yang diinput ke array nimMHS
+
+        int jumlahMatkul = 0;
+        while (true) {
+            
+            //cek apakah matkul mencapai limit
+            if (jumlahMatkul > max_matkul) {
+                System.out.println("Kapasitas matkul penuh");
+                break;
+            }
+            //cek apakah total sks seorang mhs mencapai limit
+            if (totalSks[jumlahMhs] >= 24) {
+                System.out.println("SKS sudah mencapai limit (maks 24 SKS)!");
+                break;
+            }
+
+            System.out.print("Kode Mata Kuliah : ");
+            String kodeMatkul = input.nextLine();
+
+            System.out.print("Nama Mata Kuliah : ");
+            String namaMatkul = input.nextLine();
+
+            int sks;
+            while (true) {
+                System.out.print("Jumlah SKS (1-3) : ");
+                sks = input.nextInt();
+                input.nextLine();
+                if (sks >= 1 && sks <= 3) {
+                    System.out.println("Data mata kuliah berhasil ditambahkan.");
+                    break;
+                }
+                else {
+                    System.out.println("Jumlah SKS tidak valid! Coba lagi");
+                }
+            }
+
+            kodeMatkulMhs[jumlahMhs][jumlahMatkul] = kodeMatkul; //simpan kode matkul yg diinput ke array kodeMatkulMHS
+            namaMatkulMhs[jumlahMhs][jumlahMatkul] = namaMatkul; //simpan nama matkul yg diinput ke array namaMatkulMHS
+            sksMatkulMhs[jumlahMhs][jumlahMatkul] = sks; //simpan sks yg diinput ke array sksMatkulMHS
+            totalSks[jumlahMhs] += sks; //tambah sks ke array totalSKS untuk mhs indeks ke jumlahMHS 
+            jumlahMatkul++;
+        
+            System.out.print("Tambah mata kuliah lain? (y/n) : ");
+            String pilihTambah = input.nextLine();
+            if (pilihTambah.equalsIgnoreCase("n")) {
+                break;
+            }
+
+        }
+        System.out.println("Total SKS yang diambil oleh " + namaMhs[jumlahMhs] + " : " + totalSks[jumlahMhs]); //tampilkan jumlah sks yg diambil
+
+        jumlahMhs++;
     }
 
     public static void tampilDataKRS() {
@@ -62,5 +132,4 @@ public class studycase_16_23 {
 
     }
 
-    // public static void 
 }
