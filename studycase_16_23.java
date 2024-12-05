8337788dadcb9e5ee63ecd1b78651a0df9235cf8
Syaifudinnn <mukhammad_adn_30rpl@student.smktelkom-mlg.sch.id>
2024-12-05 13:32:16 +0700

    add

--- studycase_16_23.java
+++ studycase_16_23.java
@@ -15,6 +15,52 @@ public class studycase_16_23 {
     static int[] totalSks = new int[max_mhs];
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            header();
+            int pilihMenu = input.nextInt();
+
+            switch (pilihMenu) {
+                case 1:
+                    tambahDataKRS();
+                    break;
+                case 2:
+                    tampilDataKRS();
+                    break;
+                case 3:
+                    analisisDataKRS();
+                    break;
+                case 4:
+                    System.out.println("Program Selesai");
+                    System.out.println("Terima Kasih!");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public static void header() {
+        System.out.println("=== PENCATATAN PRESTASI MAHASISWA ===");
+        System.out.println("1. Tambah Data KRS");
+        System.out.println("2. Tampilkan Daftar KRS Mahasiswa");
+        System.out.println("3. Analisis Data KRS ");
+        System.out.println("4. Keluar");
+        System.out.print("Pilih Menu : ");
+    }
+
+    public static void tambahDataKRS() {
 
     }
+
+    public static void tampilDataKRS() {
+
+    }
+
+    public static void analisisDataKRS() {
+
+    }
+
+    // public static void 
 }
